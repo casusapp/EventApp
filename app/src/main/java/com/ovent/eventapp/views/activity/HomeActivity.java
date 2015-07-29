22d3feab21554c4fa443ea5180aace44ebb1f469
Naman Parakh <parakhnr@gmail.com>
2015-07-29 23:33:40 +0530

    cordinator layout

--- app/src/main/java/com/ovent/eventapp/views/activity/HomeActivity.java
+++ app/src/main/java/com/ovent/eventapp/views/activity/HomeActivity.java
@@ -1,14 +1,22 @@
 package com.ovent.eventapp.views.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
-
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import com.ovent.eventapp.R;
 import com.ovent.eventapp.views.fragments.HomeNavigationFragment;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -17,11 +25,13 @@ public class HomeActivity extends AppCompatActivity{
 
 
 
-    @Bind(R.id.DlHome)
+  /*  @Bind(R.id.DlHome)
     DrawerLayout mDrawerLayout;
-
-    @Bind(R.id.appbar)
+*/
+    @Bind(R.id.toolbar)
     Toolbar toolbar;
+
+    //@Bind(R.id.TabView) TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,35 +39,13 @@ public class HomeActivity extends AppCompatActivity{
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        HomeNavigationFragment navigationFragment =
+        //getSupportActionBar().setHomeButtonEnabled(true);
+       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    /* HomeNavigationFragment navigationFragment =
                 (HomeNavigationFragment)getFragmentManager().findFragmentById(R.id.FgHomeNavigation);
-        navigationFragment.setUp(mDrawerLayout,toolbar);
+        navigationFragment.setUp(mDrawerLayout,toolbar);*/
 
+       toolbar.setTitle("This works");
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
